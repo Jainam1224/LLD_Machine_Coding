abe72458649c583f0018e313ae347c66e2e0ad74
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-07-12 15:17:04 +0530

    progress bar implementation - accessibility added, scalable by passing extra props

--- progress-bar/components/ProgressBar.jsx
+++ progress-bar/components/ProgressBar.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-const ProgressBar = () => {
-  return <div>ProgressBar</div>;
-};
+const MIN = 0;
+const MAX = 100;
 
-export default ProgressBar;
+export default function ProgressBar({ value = 0, onComplete = () => {} }) {
+  const [percent, setPercent] = useState(value);
+
+  useEffect(() => {
+    setPercent(Math.min(Math.max(value, MIN), MAX));
+
+    if (value >= MAX) {
+      onComplete();
+    }
+  }, [value]);
+
+  return (
+    <div className="progress">
+      <span
+        style={{
+          color: percent > 49 ? "white" : "black",
+        }}
+      >
+        {percent.toFixed()}%
+      </span>
+      <div
+        // style={{ width: `${percent}%` }}
+        style={{
+          transform: `scaleX(${percent / MAX})`,
+          transformOrigin: "left",
+        }}
+        aria-valuemin={MIN} // accessibility attributes
+        aria-valuemax={MAX} // accessibility attributes
+        aria-valuenow={percent} // accessibility attributes
+        role="progressbar"
+      />
+    </div>
+  );
+}
--- progress-bar/src/App.jsx
+++ progress-bar/src/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import ProgressBar from "../components/ProgressBar";
 
-function App() {
-  return <ProgressBar />;
-}
+export default function App() {
+  const [value, setValue] = useState(0);
+  const [success, setSuccess] = useState(false);
 
-export default App;
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 0.1);
+    }, 20);
+  }, []);
+
+  return (
+    <div className="app">
+      <span>Progress Bar</span>
+      <ProgressBar value={value} onComplete={() => setSuccess(true)} />
+      <span>{success ? "Complete!" : "Loading..."}</span>
+    </div>
+  );
+}
