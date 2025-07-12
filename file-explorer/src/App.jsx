6d1fe47c0b1899abb5c32f71e8f7c10945d7eafe
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-07-12 13:07:37 +0530

    insert new folder or file implementation

--- file-explorer/src/App.jsx
+++ file-explorer/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 import Folder from "../components/folder";
 import explorer from "../data/data";
 import "./App.css";
+import useTraverseTree from "../hooks/useTraverseTree";
 
 export default function App() {
   const [explorerData, setExplorerData] = useState(explorer);
 
+  const { insertNode } = useTraverseTree();
+
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item, isFolder);
+    setExplorerData(finalTree);
+  };
+
   return (
     <div className="App">
-      <Folder explorer={explorerData} handleInsertNode={() => {}} />
+      <Folder handleInsertNode={handleInsertNode} explorer={explorerData} />
     </div>
   );
 }
