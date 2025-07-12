41ee56d6c5a709b0b1ac83b737255a70c52f06c0
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-07-12 13:39:01 +0530

    backend driven pagination using skip query

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import Pagination from "../components/Pagination";
+import PaginationBackendDriven from "../components/PaginationBackendDriven";
 
 function App() {
-  return <Pagination />;
+  // return <Pagination />;
+  return <PaginationBackendDriven />;
 }
 
 export default App;
