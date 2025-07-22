6ea39b1829e03d8ad27a5fd0ac2fa78068c22d50
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-07-23 02:24:17 +0530

    position implementation of the toast

--- toast-component/components/Notification.jsx
+++ toast-component/components/Notification.jsx
@@ -17,11 +17,8 @@ const icons = {
 const Notification = ({ type = "info", message, onClose = () => {} }) => {
   return (
     <div className={`notification ${type}`}>
-      {/* icon */}
       {icons[type]}
-      {/* message */}
       {message}
-      {/* close button */}
       <AiOutlineClose
         color="white"
         className="closeBtn"
