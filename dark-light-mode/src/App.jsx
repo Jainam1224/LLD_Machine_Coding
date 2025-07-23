dfbfe903c2b90edf8cb7fe4c740aaef743d6034f
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-07-23 13:53:07 +0530

    page routes and navbar ui created

--- dark-light-mode/src/App.jsx
+++ dark-light-mode/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Blog from "../pages/Blog";
+import Navbar from "../component/NavBar";
 
 function App() {
-  return <div>DARK-LIGHT-MODE</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
